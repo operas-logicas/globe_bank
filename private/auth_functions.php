e6efebb0c0599e813eb446e1001b2a35f9a4bf37
Robert Miller <miller.robert.john@gmail.com>
2021-01-04 12:45:26 -0700

    Added admin levels to auth_functions and admins/index.php

--- private/auth_functions.php
+++ private/auth_functions.php
@@ -5,6 +5,7 @@
   // Renerating the ID protects the admin from session fixation.
     session_regenerate_id();
     $_SESSION['admin_id'] = $admin['id'];
+    $_SESSION['admin_level'] = $admin['level'];
     $_SESSION['last_login'] = time();
     $_SESSION['username'] = $admin['username'];
     return true;
@@ -25,6 +26,7 @@
   // Performs all actions necessary to log out an admin
   function log_out_admin() {
     unset($_SESSION['admin_id']);
+    unset($_SESSION['admin_level']);
     unset($_SESSION['last_login']);
     unset($_SESSION['username']);
     // Optional:
@@ -33,7 +35,7 @@
   }
 
   // Call require_login() at the top of any page which needs to
-  // require a valid login before granting acccess to the page.
+  // require a valid login (user level access [>= 0] )
   function require_login() {
     if (!is_logged_in()) {
       redirect_to(url_for('/staff/login.php'));
@@ -42,6 +44,54 @@
     }
   }
 
+  // Call require_admin() at the top of any page which needs to
+  // require a valid login AND at least admin level access (>= 400)
+  function require_admin() {
+    require_login();
+    if (!$_SESSION['admin_level'] >= 400) {
+      redirect_to(url_for('/staff/index.php'));
+    } else {
+      // Do nothing, let the rest of the pae proceed
+    }
+  }
 
+// Call require_superuser() at the top of any page which needs to
+// require a valid login AND at least superuser level access (>= 800)
+function require_superuser()
+{
+  require_login();
+  if (!$_SESSION['admin_level'] >= 800) {
+    redirect_to(url_for('/staff/index.php'));
+  } else {
+    // Do nothing, let the rest of the pae proceed
+  }
+}
+
+// Returns logged in user's admin id
+function get_user_id()
+{
+  return $_SESSION['admin_id'] ?? 0;
+}
+
+// Returns logged in user's admin level
+function get_user_level() {
+  return $_SESSION['admin_level'] ?? 0;
+}
+
+// Returns user type based on provided access level
+function get_user_type($level) {
+  switch ($level) {
+    case '800':
+      $level = 'Superuser';
+      break;
+    case '400':
+      $level = 'Admin';
+      break;
+    case '0':
+    default:
+      $level = 'User';
+  }
+  return $level;
+}
 
 ?>
--- public/staff/admins/index.php
+++ public/staff/admins/index.php
@@ -8,16 +8,18 @@ $result = find_all_admins();
 
 ?>
 
-<?php $page_title = 'Admins'; ?>
+<?php $page_title = 'Users'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
   <div class="admins listing">
-    <h1>Admins</h1>
+    <h1>Users</h1>
 
-    <div class="actions">
-      <a class="action" href="<?php echo url_for('/staff/admins/new.php'); ?>">Create New Admin</a>
-    </div>
+    <?php if(get_user_level() >= 400) { ?>
+      <div class="actions">
+        <a class="action" href="<?php echo url_for('/staff/admins/new.php'); ?>">Create New User</a>
+      </div>
+    <?php } ?>
 
     <table class="list">
       <tr>
@@ -26,21 +28,31 @@ $result = find_all_admins();
         <th>Last Name</th>
         <th>Email</th>
         <th>Username</th>
+        <th>User Level</th>
         <th>&nbsp;</th>
         <th>&nbsp;</th>
         <th>&nbsp;</th>
       </tr>
 
-      <?php while ($admin = mysqli_fetch_assoc($result)) { ?>
+      <?php while($admin = mysqli_fetch_assoc($result)) { ?>
         <tr>
           <td><?php echo h($admin['id']); ?></td>
           <td><?php echo h($admin['first_name']); ?></td>
           <td><?php echo h($admin['last_name']); ?></td>
           <td><?php echo h($admin['email']); ?></td>
           <td><?php echo h($admin['username']); ?></td>
+          <td><?php echo h(get_user_type($admin['level'])); ?></td>
           <td><a class="action" href="<?php echo url_for('/staff/admins/show.php?id=' . h(u($admin['id']))); ?>">View</a></td>
-          <td><a class="action" href="<?php echo url_for('/staff/admins/edit.php?id=' . h(u($admin['id']))); ?>">Edit</a></td>
-          <td><a class="action" href="<?php echo url_for('/staff/admins/delete.php?id=' . h(u($admin['id']))); ?>">Delete</a></td>
+          <td>
+            <?php if(get_user_level() > $admin['level'] || get_user_id() === $admin['id']) { ?>
+              <a class="action" href="<?php echo url_for('/staff/admins/edit.php?id=' . h(u($admin['id']))); ?>">Edit</a>
+            <?php } else { echo '&nbsp;'; } ?>
+          </td>
+          <td>
+            <?php if(get_user_level() > $admin['level'] || get_user_id() === $admin['id']) { ?>
+              <a class="action" href="<?php echo url_for('/staff/admins/delete.php?id=' . h(u($admin['id']))); ?>">Delete</a>
+            <?php } else { echo '&nbsp;'; } ?>
+          </td>
         </tr>
       <?php } ?>
     </table>
